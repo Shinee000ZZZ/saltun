1eb7f95d8643d7ebb9fc42cac5cdc03ef2199625
Shineeee <shineggod@gmail.com>
2024-05-20 13:55:12 +0700

    edit dikit

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -3,14 +3,14 @@ import styles from './Contact.module.css'
 
 export const Contact = () => {
     return (
-    <footer className={styles.container}>
+    <footer className={styles.container} id="contact">
         <div>
             <h2 className={styles.title}>Contact Me</h2>
             <ul className={styles.socials}>
-                <a href="https://www.instagram.com/kesaltunan/" target="_blank"><img src="assets/Instagram.png" alt="" className={styles.ig}/></a>
-                <a href="https://www.tiktok.com/@oshitergantungfyp" target='_blank'><img src="assets/tiktok.png" alt="" className={styles.tiktok} /></a>
-                <a href="https://x.com/OkeShinee" target='_blank'><img src="assets/twitterx.png" alt="" className={styles.twitter}/></a>
-                <a href="https://github.com/Shinee000ZZZ" target='_blank'><img src="assets/github.png" alt="" className={styles.github}/></a>
+                <a href="https://www.instagram.com/kesaltunan/" target="_blank"><img src="assets/contact/Instagram.png" alt="" className={styles.ig}/></a>
+                <a href="https://www.tiktok.com/@oshitergantungfyp" target='_blank'><img src="assets/contact/tiktok.png" alt="" className={styles.tiktok} /></a>
+                <a href="https://x.com/OkeShinee" target='_blank'><img src="assets/contact/twitterx.png" alt="" className={styles.twitter}/></a>
+                <a href="https://github.com/Shinee000ZZZ" target='_blank'><img src="assets/contact/github.png" alt="" className={styles.github}/></a>
             </ul>
         </div>
         <div className={styles.copyright}>
